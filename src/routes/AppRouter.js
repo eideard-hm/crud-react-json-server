35d9b52ead704ce9cb1ecca331830b6a5b2bbeb4
= <=>
2021-09-24 09:16:28 -0500

    Se implemento la función de registrar, utilizando la API de json server que permite simular una API real.

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Header from "../components/Header"
 import HomePage from "../pages/HomePage"
+import NewMotorcyclePage from "../pages/NewMotorcyclePage"
 
 const AppRoutes = () => {
     return (
         <Router>
+            <Header brand="React App" />
             <Switch>
                 <Route path="/" exact={true} component={HomePage} />
+                <Route path="/new" exact={true} component={NewMotorcyclePage} />
             </Switch>
         </Router>
     )
